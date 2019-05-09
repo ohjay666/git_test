b6caf57484a592e90a4384104800e6fe8b5eb93a
oj <oj@rutan019.fritz.box>
2019-05-09 16:07:44 +0200

    fertig

---
+++ public_html/funcs.js
@@ -0,0 +1,5 @@
+//document.getElementsByName('body')[0].backgroundColor = 'black';
+setTimeout(function () {
+	document.getElementsByTagName('body')[0].style.backgroundColor = 'black';
+	//document.getElementsByTagName('body').backgroundColor = 'black';
+}, 1000)
